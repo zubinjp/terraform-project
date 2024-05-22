5f189e9e54aac979cfaf336d76a9394f71a0dc09
zubinjp <zubinjp@gmail.com>
2024-05-22 19:20:32 -0400

    Output Commit

--- modules/app_service/outputs.tf
+++ modules/app_service/outputs.tf
@@ -1,4 +1,4 @@
 output "app_service_default_hostname" {
-  description = "The default hostname of the App Service."
+  description = "Name of the App Service."
   value       = azurerm_app_service.example.default_site_hostname
 }
