1b5a4a55661dda04e25ee0eb3001ec8b3168dd3c
zubinjp <zubinjp@gmail.com>
2024-05-24 17:35:09 -0400

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -1,18 +1,6 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_app_service" "app" {
+resource "azurerm_app_service" "example" {
   name                = var.app_service_name
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = var.app_service_plan_id
-}
-
-output "app_service_id" {
-  value = azurerm_app_service.app.id
-}
-
-output "app_service_url" {
-  value = azurerm_app_service.app.default_site_hostname
 }
