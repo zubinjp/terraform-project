e9f53511a08402ded4caf287fc67937eaec51c1a
zubinjp <zubinjp@gmail.com>
2024-05-24 16:24:17 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,25 @@
-variable "app_service_name" {
-  description = "The name of the App Service."
-  type        = string
+variable "client_id" {
+  type = string
 }
 
-variable "location" {
-  description = "The location where resources will be created."
-  type        = string
-  default     = "East US"
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-  default     = "PROJECT3"
+  type    = string
+  default = "myResourceGroup"
 }
 
-variable "app_service_plan_name" {
-  description = "The name of the App Service Plan."
-  type        = string
-  default     = "ASP-PROJECT3-8a65"
-}
-
-variable "app_service_plan_id" {
-  description = "The ID of the App Service Plan."
-  type        = string
-  default     = "YOUR_DEFAULT_APP_SERVICE_PLAN_ID"
+variable "location" {
+  type    = string
+  default = "East US"
 }
