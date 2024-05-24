f2ca1a2c954423070f6e003605c957601c8271e1
zubinjp <zubinjp@gmail.com>
2024-05-24 17:16:17 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "app_service_default_hostname" {
-  description = "Name of the App Service."
-  value       = module.app_service.app_service_default_hostname
+output "app_service_id" {
+  description = "The ID of the deployed App Service"
+  value       = module.app_service.app_service_id
+}
+
+output "app_service_url" {
+  description = "The URL of the deployed App Service"
+  value       = module.app_service.app_service_url
 }
