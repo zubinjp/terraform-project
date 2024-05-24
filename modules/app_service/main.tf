36fbb04cc19f74a05c259ee10020aa269ac397b3
zubinjp <zubinjp@gmail.com>
2024-05-24 17:15:25 -0400

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -1,10 +1,18 @@
-resource "azurerm_app_service" "example" {
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_app_service" "app" {
   name                = var.app_service_name
   location            = var.location
   resource_group_name = var.resource_group_name
   app_service_plan_id = var.app_service_plan_id
+}
 
-  site_config {
-    windows_fx_version = ".NET|8.0"
-  }
+output "app_service_id" {
+  value = azurerm_app_service.app.id
+}
+
+output "app_service_url" {
+  value = azurerm_app_service.app.default_site_hostname
 }
