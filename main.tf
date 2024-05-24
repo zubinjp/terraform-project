bb1f5069ba70ebd2da00e6836ef14ce30589885f
zubinjp <zubinjp@gmail.com>
2024-05-23 22:22:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "azurerm_app_service_plan" "example" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
   kind                = "Windows"
 
   sku {
