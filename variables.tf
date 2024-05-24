56d767d30f00db5e4ef609dd3fdb6fb3d160dcef
zubinjp <zubinjp@gmail.com>
2024-05-24 16:49:28 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,25 +1,39 @@
 variable "client_id" {
-  type = string
+  description = "The client ID of the Azure service principal"
+  type        = string
 }
 
 variable "client_secret" {
-  type = string
+  description = "The client secret of the Azure service principal"
+  type        = string
 }
 
 variable "subscription_id" {
-  type = string
+  description = "The subscription ID in Azure"
+  type        = string
 }
 
 variable "tenant_id" {
-  type = string
+  description = "The tenant ID in Azure"
+  type        = string
 }
 
 variable "resource_group_name" {
-  type    = string
-  default = "myResourceGroup"
+  description = "The name of the resource group"
+  type        = string
 }
 
 variable "location" {
-  type    = string
-  default = "East US"
+  description = "The location/region where the resources will be created"
+  type        = string
+}
+
+variable "app_service_name" {
+  description = "The name of the app service"
+  type        = string
+}
+
+variable "app_service_plan_name" {
+  description = "The name of the app service plan"
+  type        = string
 }
