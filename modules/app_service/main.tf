61d7bc3289e4d002d7a1f3675f116e6436cebdbc
zubinjp <zubinjp@gmail.com>
2024-05-24 18:25:14 -0400

    Update main.tf

--- modules/app_service/main.tf
+++ modules/app_service/main.tf
@@ -2,8 +2,4 @@ resource "azurerm_app_service" "example" {
   name                = var.app_service_name
   location            = var.location
   app_service_plan_id = var.app_service_plan_id
-
-  app_settings = {
-    "WEBSITE_NODE_DEFAULT_VERSION" = "14.17.0"
-  }
 }
