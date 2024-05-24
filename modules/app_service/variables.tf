ac095cb6ebb17bca2f888bb0a2f358263d2c6dbc
zubinjp <zubinjp@gmail.com>
2024-05-24 18:30:24 -0400

    Create variables.tf

---
+++ modules/app_service/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+}
+
+variable "location" {
+  description = "The location/region where the resources will be created"
+}
+
+variable "app_service_name" {
+  description = "The name of the app service"
+}
+
+variable "app_service_plan_id" {
+  description = "The ID of the app service plan"
+}
