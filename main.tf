a516f51b3f74a9ada44346454db67baf3709fb42
zubinjp <zubinjp@gmail.com>
2024-05-24 23:59:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_app_service_plan" "example" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  kind                = "Windows"  # Or "Linux", depending on your requirements
+  kind                = "Windows"  
 
   sku {
     tier = "Free"
@@ -24,7 +24,7 @@ module "app_service" {
   resource_group_name = var.resource_group_name
   location            = var.location
   app_service_name    = var.app_service_name
-  app_service_plan_id = var.app_service_plan_id
+  app_service_plan_id = azurerm_service_plan.example.id
 }
 
 output "service_plan_id" {
