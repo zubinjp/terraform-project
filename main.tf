715ac39e86080420c9932f25b821cac376822e7a
zubinjp <zubinjp@gmail.com>
2024-05-24 23:05:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "azurerm_service_plan" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   kind                = "Windows"
+os_type             = "Windows"
 
   sku {
     tier = "Free"
