28418990a95f63046c5109bf3a1004e483037d02
zubinjp <zubinjp@gmail.com>
2024-05-25 18:19:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_app_service_plan" "function_app" {
   resource_group_name = azurerm_resource_group.example.name
   kind                = "Linux"
   reserved            = true
-  is_xenon            = true  
+  is_xenon            = true  # Adding is_xenon attribute here
 
   sku {
     tier     = "Standard"
@@ -29,7 +29,7 @@ resource "azurerm_app_service_plan" "web_app" {
   resource_group_name = azurerm_resource_group.example.name
   kind                = "Linux"
   reserved            = true
-  is_xenon            = true 
+  is_xenon            = true  # Adding is_xenon attribute here
 
   sku {
     tier     = "Standard"
@@ -43,8 +43,7 @@ module "app_service" {
   resource_group_name             = var.resource_group_name
   location                        = var.location
   app_service_name                = var.app_service_name
-  function_app_service_plan_id    = azurerm_app_service_plan.function_app.id
-  web_app_service_plan_id         = azurerm_app_service_plan.web_app.id
+  app_service_plan_id             = azurerm_app_service_plan.function_app.id
 }
 
 output "function_app_service_plan_id" {
