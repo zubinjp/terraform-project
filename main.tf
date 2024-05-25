60d9221af1bfc47b8aff66d1025ffdbce595f207
zubinjp <zubinjp@gmail.com>
2024-05-25 18:02:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "example" {
 
 resource "azurerm_app_service_plan" "example" {
   name                = var.app_service_plan_name
-  location            = "norwayeast"
+  location            = "switzerlandnorth"
   resource_group_name = azurerm_resource_group.example.name
   kind                = "Linux"
   reserved            = true
