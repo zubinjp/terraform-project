bcaff2f57260416e7f4dbb182a56dd40b2102a9b
zubinjp <zubinjp@gmail.com>
2024-05-24 20:29:02 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "app_service_url" {
-  description = "The URL of the deployed Azure App Service"
-  value       = azurerm_app_service.example.default_site_hostname
-}
-
 output "resource_group_name" {
-  description = "The name of the Azure Resource Group"
+  description = "The name of the resource group created."
   value       = azurerm_resource_group.example.name
 }
 
-output "location" {
-  description = "The location of the Azure resources"
+output "resource_group_location" {
+  description = "The location of the resource group created."
   value       = azurerm_resource_group.example.location
 }
 
-output "app_service_plan_id" {
-  description = "The ID of the Azure App Service Plan"
-  value       = azurerm_app_service_plan.example.id
+output "service_plan_id" {
+  description = "The ID of the Azure App Service Plan."
+  value       = azurerm_service_plan.example.id
+}
+
+output "app_service_url" {
+  description = "The URL of the Azure App Service."
+  value       = module.app_service.app_service_url
 }
