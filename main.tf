84ad03bfadb4169e3962479eb4882fbb48f03f1b
zubinjp <zubinjp@gmail.com>
2024-05-25 18:13:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,9 +8,24 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = var.app_service_plan_name
-  location            = "eastus"
+resource "azurerm_app_service_plan" "function_app" {
+  name                = var.function_app_service_plan_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
+  is_xenon            = true  # Adding is_xenon attribute here
+
+  sku {
+    tier     = "Standard"
+    size     = "S1"
+    capacity = 1
+  }
+}
+
+resource "azurerm_app_service_plan" "web_app" {
+  name                = var.web_app_service_plan_name
+  location            = var.location
   resource_group_name = azurerm_resource_group.example.name
   kind                = "Linux"
   reserved            = true
@@ -28,9 +43,14 @@ module "app_service" {
   resource_group_name = var.resource_group_name
   location            = var.location
   app_service_name    = var.app_service_name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  function_app_service_plan_id = azurerm_app_service_plan.function_app.id
+  web_app_service_plan_id      = azurerm_app_service_plan.web_app.id
 }
 
-output "service_plan_id" {
-  value = azurerm_app_service_plan.example.id
+output "function_app_service_plan_id" {
+  value = azurerm_app_service_plan.function_app.id
+}
+
+output "web_app_service_plan_id" {
+  value = azurerm_app_service_plan.web_app.id
 }
