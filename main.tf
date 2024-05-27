0931cb58834380fa35d9392e39bf1ebb0333a231
zubinjp <zubinjp@gmail.com>
2024-05-27 17:34:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,21 +8,21 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
+  name                = "pjt3-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
+  name                 = "pjt3-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
+  name                = "pjt3-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
@@ -34,7 +34,7 @@ resource "azurerm_network_interface" "example" {
 }
 
 resource "azurerm_network_security_group" "example" {
-  name                = "example-nsg"
+  name                = "pjt3-nsg"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
